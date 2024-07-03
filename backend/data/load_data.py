e3663dbf893120e08e7872c8f5c4613cb0bc61f5
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-07-03 00:51:15 -0400

    connected load data to database

--- backend/data/load_data.py
+++ backend/data/load_data.py
@@ -2,3 +2,9 @@ import pandas as pd
 from sqlalchemy import create_engine
 import os
 
+# database connection
+db_user = os.getenv('POSTGRES_USER')
+db_password = os.getenv('POSTGRES_PASSWORD')
+db_name = os.getenv('POSTGRES_DB')
+db_host = 'db'
+engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}')

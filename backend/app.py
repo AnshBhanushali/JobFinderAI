ddbbc6b13634b5d976ecc6be109cf43748ff8535
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-07-02 23:37:43 -0400

    added all dependencies

---
+++ backend/app.py
@@ -0,0 +1,6 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sqlalchemy import create_engine

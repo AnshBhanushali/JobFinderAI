fe6187a6a08ab60a2f351f2918d843d75221edcd
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-07-03 01:00:20 -0400

    handling csv data

--- backend/data/load_data.py
+++ backend/data/load_data.py
@@ -8,3 +8,20 @@ db_password = os.getenv('POSTGRES_PASSWORD')
 db_name = os.getenv('POSTGRES_DB')
 db_host = 'db'
 engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}')
+
+# data from CSV
+jobs_df = pd.read_csv('backend/data/jobs.csv')
+
+# Manipulating data from csv
+jobs_df = jobs_df[['Job Title', 'Key Skills', 'Role Category', 'Location']]
+jobs_df.columns = ['title', 'description', 'company', 'location']
+
+jobs_df['company'] = jobs_df['company'] + " - " + jobs_df['description']
+
+jobs_df.drop_duplicates(inplace=True)
+
+jobs_df.to_sql('jobs', engine, if_exists='replace', index=False)
+
+print("Data has been successfully inserted into the database.")
+
+

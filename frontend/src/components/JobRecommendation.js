9d1bf19e8f1a15f30a621de01876262039f6e38e
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-07-03 01:31:10 -0400

    enhanced main page

--- frontend/src/components/JobRecommendation.js
+++ frontend/src/components/JobRecommendation.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './JobRecommendation.css';
+import './JobRecommendation.css'; 
 
 const JobRecommendation = () => {
     const [resume, setResume] = useState('');

dfccb0b7a0a4945df7775fe590b6cd66c2241469
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-07-02 23:48:09 -0400

    created requirements

--- backend/app.py
+++ backend/app.py
@@ -4,3 +4,6 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sqlalchemy import create_engine
+
+app = Flask(__name__)
+CORS(app)

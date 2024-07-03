569ef384274ab6d2dc3372ad041370a37bca94f3
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-07-03 02:13:21 -0400

    final code cleanup

--- backend/data/load_data.py
+++ backend/data/load_data.py
@@ -1,18 +1,23 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import os
+from dotenv import load_dotenv
 
-# database connection
+load_dotenv()
+
 db_user = os.getenv('POSTGRES_USER')
 db_password = os.getenv('POSTGRES_PASSWORD')
 db_name = os.getenv('POSTGRES_DB')
-db_host = 'db'
+db_host = 'localhost'  # Use 'localhost' since Docker is not used
+
+if not db_user or not db_password or not db_name:
+    raise Exception("Database environment variables not set correctly")
+
+
 engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}')
 
-# data from CSV
-jobs_df = pd.read_csv('backend/data/jobs.csv')
+jobs_df = pd.read_csv('data/jobs.csv')
 
-# Manipulating data from csv
 jobs_df = jobs_df[['Job Title', 'Key Skills', 'Role Category', 'Location']]
 jobs_df.columns = ['title', 'description', 'company', 'location']
 
@@ -23,5 +28,3 @@ jobs_df.drop_duplicates(inplace=True)
 jobs_df.to_sql('jobs', engine, if_exists='replace', index=False)
 
 print("Data has been successfully inserted into the database.")
-
-
--- backend/models/job_recomModel.py
+++ backend/models/job_recomModel.py
@@ -3,17 +3,27 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sqlalchemy import create_engine
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class JobRecommendationModel:
     def __init__(self):
         db_user = os.getenv('POSTGRES_USER')
         db_password = os.getenv('POSTGRES_PASSWORD')
         db_name = os.getenv('POSTGRES_DB')
-        db_host = 'db'
+        db_host = 'localhost'  # Use 'localhost' since Docker is not used
+
+        if not db_user or not db_password or not db_name:
+            raise Exception("Database environment variables not set correctly")
+
         self.engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}')
         
         self.jobs = pd.read_sql('jobs', self.engine)
         
+        self.jobs = self.jobs.dropna(subset=['description'])
+
         self.vectorizer = TfidfVectorizer(stop_words='english')
         self.job_vectors = self.vectorizer.fit_transform(self.jobs['description'])
         
